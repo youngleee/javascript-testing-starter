9b14101d4136fc23cb726c4be200fcb6589abf77
Young Lee <youngleeee@outlook.com>
2024-06-24 15:49:07 +0200

    learnt how to unit test with vitest on node and javascript

--- src/libs/tests/intro.test.js
+++ src/libs/tests/intro.test.js
@@ -1,4 +1,6 @@
 import {describe, test, it, expect} from "vitest";
+import {max} from './src/intro'
+import {fizzBuzz} from './src/intro'
 
 describe('max', () => {
     it('should return the first argument if it is greater', () => {
@@ -12,5 +14,32 @@ describe('max', () => {
 
         // Assert
         expect(result).toBe(2);
+
+        //OR
+    }) 
+    it('should return the first argument if it is greater', () => {
+        expect(max(3,5)).toBe(5);
+    })
+    it('should return the first argument if it is greater', () => {
+        expect(max(3,7)).toBe(7);
+    })
+    it('should return the first argument if it is greater', () => {
+        expect(max(3,44)).toBe(44);
+    })
+})
+
+describe('fizzBuzz', () => {
+    it('should return FizzBuzz if n is divisible by 3 AND 5)', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    })
+
+    it('should return Fizz if n is divisible only by 3)', () => {
+        expect(fizzBuzz(3)).toBe('Fizz');
+    })
+    it('should return Buzz if n is divisible only by 5)', () => {
+        expect(fizzBuzz(5)).toBe('Buzz');
+    })
+    it('should return string if not divisible by both 3 and 5)', () => {
+        expect(fizzBuzz(4)).toBe('4');
     })
 })
